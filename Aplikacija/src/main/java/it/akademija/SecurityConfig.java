e882322d4765f473a8cc6d2251aea3933b795b6d
aurelija <a.drobelyte@gmail.com>
2020-01-28 11:48:22 +0200

    before redoing

--- Aplikacija/src/main/java/it/akademija/SecurityConfig.java
+++ Aplikacija/src/main/java/it/akademija/SecurityConfig.java
@@ -14,17 +14,77 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import it.akademija.SecurityEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Autowired
+//	private SecurityEntryPoint securityEntryPoint;
+//	@Autowired
+//	private UserDetailsService userService;
+//
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(userService);
+//
+//	}
+//
+//	@Bean
+//	public PasswordEncoder getPasswordEncoder() {
+//		return NoOpPasswordEncoder.getInstance();
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//				// be saugumo UI dalis ir swaggeris
+//				.antMatchers("/", "/swagger-ui.html", "/login*").permitAll()
+//				// visi /test/ keliai yra saugus, pasiekiami tik prisijungus
+//				.antMatchers("/user/**", "/admin/**", "/").authenticated().and().formLogin().permitAll()
+//
+//				// prisijungus
+//				.successHandler(new AuthenticationSuccessHandler() {
+//					@Override
+//					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+//							Authentication authentication) throws IOException, ServletException {
+//						response.setHeader("Access-Control-Allow-Credentials", "true");
+//						response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+//						response.setHeader("Content-Type", "application/json;charset=UTF-8");
+//						response.getWriter().print("{\"username\": \""
+//								+ SecurityContextHolder.getContext().getAuthentication().getName() + "\"}");
+//					}
+//				})
+//				// esant blogiems user/pass
+//				.failureHandler(new SimpleUrlAuthenticationFailureHandler()).loginPage("/").permitAll().and().logout()
+//				.permitAll()
+//				// G pridetas testavimui. veikia. tik nzn, ka reikia paduoti i response. idejau
+//				// ta pati ka i login
+//				.logoutSuccessHandler(new LogoutSuccessHandler() {
+//					@Override
+//					public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+//							Authentication authentication) throws IOException, ServletException {
+//						response.setHeader("Access-Control-Allow-Credentials", "true");
+//						response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+//						response.setHeader("Content-Type", "application/json;charset=UTF-8");
+//					}
+//				}).and().csrf().disable() // nenaudojam tokenu
+//				// forbidden klaidai
+//				.exceptionHandling().authenticationEntryPoint(securityEntryPoint).and().headers().frameOptions()
+//				.disable(); // H2 konsolei
+//
+//	}
 
 	@Autowired
 	private SecurityEntryPoint securityEntryPoint;
@@ -46,28 +106,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				// be saugumo UI dalis ir swaggeris
-				.antMatchers("/swagger-ui.html", "/console", "/api/**").permitAll()
+				.antMatchers("/**", "/swagger-ui.html", "/console", "/api/**", "/admin/**").permitAll()
+				// .antMatchers("/user/**").authenticated()
+				// .and()
 				// visi /api/ saugus (dar galima .anyRequest() )
-				.antMatchers("/user").access("hasRole('ROLE_USER')").antMatchers("/admin")
-				.access("hasRole('ROLE_ADMIN')") // admin
-				.antMatchers("/").authenticated().and().formLogin() // leidziam login
+				.antMatchers("/user/**", "/admin/**").authenticated().and().formLogin().loginPage("/login").permitAll() // leidziam
+				// login
 				// prisijungus
 
-				.successHandler(new AuthenticationSuccessHandler() {
+				// .antMatchers("/test/**").authenticated().and().formLogin().loginPage("/login").permitAll(
 
+				.successHandler(new AuthenticationSuccessHandler() {
 					@Override
 					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-							org.springframework.security.core.Authentication authentication)
-							throws IOException, ServletException {
+							Authentication authentication) throws IOException, ServletException {
 						response.setHeader("Access-Control-Allow-Credentials", "true");
 						response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 						response.setHeader("Content-Type", "application/json;charset=UTF-8");
 						response.getWriter().print("{\"username\": \""
 								+ SecurityContextHolder.getContext().getAuthentication().getName() + "\"}");
-
 					}
 
-				})
+				}).defaultSuccessUrl("/admin", true)
 				// esant blogiems user/pass
 				.failureHandler(new SimpleUrlAuthenticationFailureHandler()).loginPage("/login").permitAll() // jis jau
 																												// egzistuoja
