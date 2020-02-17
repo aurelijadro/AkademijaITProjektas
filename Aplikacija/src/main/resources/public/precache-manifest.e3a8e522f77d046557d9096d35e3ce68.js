6424de8f9c9f093176d891a4993018ed42104cdb
aurelija <a.drobelyte@gmail.com>
2020-02-17 11:38:35 +0200

    created react context

--- Aplikacija/src/main/resources/public/precache-manifest.e3a8e522f77d046557d9096d35e3ce68.js
+++ Aplikacija/src/main/resources/public/precache-manifest.e3a8e522f77d046557d9096d35e3ce68.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef33741000efcbf0b85d8065fbaf80b0",
+    "revision": "c13e8a6dbca2ccddf320f3f00dd108fa",
     "url": "./Gentoo/index.html"
   },
   {
-    "revision": "6c3ea0936ab2d439d987",
+    "revision": "d4b9d593873645877ace",
     "url": "./Gentoo/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "0fb0cad7362775044218",
+    "revision": "5e661ce3e0735e7aa114",
     "url": "./Gentoo/static/css/main.bf645f0c.chunk.css"
   },
   {
-    "revision": "6c3ea0936ab2d439d987",
-    "url": "./Gentoo/static/js/2.d55ada08.chunk.js"
+    "revision": "d4b9d593873645877ace",
+    "url": "./Gentoo/static/js/2.0e6f5b78.chunk.js"
   },
   {
-    "revision": "0fb0cad7362775044218",
-    "url": "./Gentoo/static/js/main.1b7bc78b.chunk.js"
+    "revision": "5e661ce3e0735e7aa114",
+    "url": "./Gentoo/static/js/main.b994ae77.chunk.js"
   },
   {
     "revision": "2678f9bb60d6db5e8ac1",
