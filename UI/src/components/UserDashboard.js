e0575076f3ac929ef0c3f8067461bd108524771f
aurelija <a.drobelyte@gmail.com>
2020-02-26 15:58:39 +0200

    working on user dashboard

--- UI/src/components/UserDashboard.js
+++ UI/src/components/UserDashboard.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import NavigationForUser from "./NavigationForUser";
-import MainDocumentListForDashboard from "./Document/MainDocumentListForDashboard";
+import axios from "axios";
+import ApiUrl from "../APIURL";
+//import MainDocumentListForDashboard from "./Document/MainDocumentListForDashboard";
+
+import CreatedDocuments from "../userComponents/CreatedDocuments";
 
 class UserDashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { userId: -2 };
+  }
+
+  componentDidMount() {
+    this.getUserId();
+  }
+
+  getUserId() {
+    axios
+      .get(`${ApiUrl}loggedUserId`)
+      .then(resp => this.setState({ userId: resp.data }));
+  }
+
   render() {
+    if (this.state.userId === -2) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
         <NavigationForUser />
-        <div className="container">
-          <MainDocumentListForDashboard />
+        <div className="container my-4">
+          <CreatedDocuments id={this.state.userId} />
         </div>
-        <div className="row"></div>
-        <hr />
       </div>
     );
   }
---
+++ UI/src/userComponents/CreatedDocuments.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ApiUrl from "../APIURL.jsx";
+import { Redirect, Link } from "react-router-dom";
+
+const CreatedDocuments = props => {
+  const userId = props.id;
+  const [createdDocuments, setCreatedDocuments] = useState("loading");
+  const [submittedDocuments, setSubmittedDocuments] = useState("loading");
+  const [saving, setSaving] = useState(false);
+
+  function fetchFromServer(path) {
+    console.log(props);
+    return axios.get(ApiUrl + path).then(resp => resp.data);
+  }
+
+  const updateCachedData = () => {
+    fetchFromServer(`documents/${userId}/createdDocuments
+    `).then(setCreatedDocuments);
+    fetchFromServer(`documents/${userId}/submittedDocuments
+    `).then(setSubmittedDocuments);
+  };
+
+  useEffect(function() {
+    updateCachedData();
+    // const timer = setInterval(updateCachedData, 2000);
+    // return () => clearInterval(timer);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function manageChanges(f) {
+    setSaving(true);
+    f()
+      .then(updateCachedData)
+      .then(() => setSaving(false));
+  }
+
+  if (userId === -2 || createdDocuments === "loading")
+    return <div>Loading...</div>;
+
+  if (userId === -1) {
+    return <Redirect to="/Gentoo" />;
+  }
+
+  const createdDocumentsList = createdDocuments.map((document, index) => {
+    function submitDocument() {
+      manageChanges(() =>
+        axios.post(
+          `${ApiUrl}documents/${userId}/${document.id}/submittedStatusUpdate`
+        )
+      );
+    }
+    function deleteDocumet() {
+      manageChanges(() =>
+        axios.delete(`${ApiUrl}documents/${userId}/${document.id}`)
+      );
+    }
+    return (
+      <li className="list-group-item list-group-item-dark" key={document.id}>
+        <div className="row my-1">
+          <div className="col-1">{index + 1}</div>
+          <div className="col-2">{document.title}</div>
+          <div className="col-2">{document.doctypes.title}</div>
+          <Link to={`/Gentoo/user`}>
+            <button
+              type="button"
+              className="btn btn-secondary btn-lg btn-block"
+            >
+              Peržiūrėti/Redaguoti
+            </button>
+          </Link>
+          <button className="col-3 btn btn-dark" onClick={submitDocument}>
+            Pateikti dokumentą
+          </button>
+          <button className="col-3 btn btn-dark" onClick={deleteDocumet}>
+            Ištrinti dokumentą
+          </button>
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <div className="container my-4">
+      <h2>Sukurti dokumentai:</h2>
+    </div>
+  );
+};
+
+export default CreatedDocuments;
